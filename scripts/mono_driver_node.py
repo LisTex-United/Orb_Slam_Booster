09a23c297a351947e0273878b716344a305823d0
RodsCoimbra <rodscoimbra@gmail.com>
2025-12-05 15:25:30 +0000

    Changed to ros topics and initial test with D435

--- scripts/mono_driver_node.py
+++ scripts/mono_driver_node.py
@@ -58,24 +58,17 @@ class MonoDriver(Node):
 
         # Initialize parameters to be passed from the command line (or launch file)
         self.declare_parameter("settings_name","EuRoC")
-        self.declare_parameter("image_seq","NULL")
 
         #* Parse values sent by command line
         self.settings_name = str(self.get_parameter('settings_name').value) 
-        self.image_seq = str(self.get_parameter('image_seq').value)
 
         # DEBUG
         print(f"-------------- Received parameters --------------------------\n")
         print(f"self.settings_name: {self.settings_name}")
-        print(f"self.image_seq: {self.image_seq}")
         print()
 
         # Global path definitions
         self.home_dir = str(Path.home()) + "/ros2_test/src/ros2_orb_slam3" #! Change this to match path to your workspace
-        self.parent_dir = "TEST_DATASET" #! Change or provide path to the parent directory where data for all image sequences are stored
-        self.image_sequence_dir = self.home_dir + "/" + self.parent_dir + "/" + self.image_seq # Full path to the image sequence folder
-
-        print(f"self.image_sequence_dir: {self.image_sequence_dir}\n")
 
         # Global variables
         self.node_name = "mono_py_driver"
@@ -85,12 +78,6 @@ class MonoDriver(Node):
 
         # Define a CvBridge object
         self.br = CvBridge()
-
-        # Read images from the chosen dataset, order them in ascending order and prepare timestep data as well
-        self.imgz_seqz_dir, self.imgz_seqz, self.time_seqz = self.get_image_dataset_asl(self.image_sequence_dir, "mav0") 
-
-        print(self.image_seq_dir)
-        print(len(self.imgz_seqz))
 
         #* ROS2 publisher/subscriber variables [HARDCODED]
         self.pub_exp_config_name = "/mono_py_driver/experiment_settings" 
@@ -112,6 +99,13 @@ class MonoDriver(Node):
         self.subscribe_exp_ack_ = self.create_subscription(String, 
                                                            self.sub_exp_ack_name, 
                                                            self.ack_callback ,10)
+        
+        self.camera_img_sub_ = self.create_subscription(
+            Image,
+            '/camera/camera/color/image_raw',
+            self.camera_image_callback,
+            10
+        )
         self.subscribe_exp_ack_
 
         # Publisher to send RGB image
@@ -125,7 +119,6 @@ class MonoDriver(Node):
         self.end_frame = -1 # Default -1
         self.frame_stop = -1 # Set -1 to use the whole sequence, some positive integer to force sequence to stop, 350 test2, 736 test3
         self.show_imgz = False # Default, False, set True to see the output directly from this node
-        self.frame_id = 0 # Integer id of an image frame
         self.frame_count = 0 # Ensure we are consistent with the count number of the frame
         self.inference_time = [] # List to compute average time
 
@@ -134,28 +127,6 @@ class MonoDriver(Node):
     # ****************************************************************************************
 
     # ****************************************************************************************
-    def get_image_dataset_asl(self, exp_dir, agent_name = "mav0"):
-        """
-            Returns images and list of timesteps in ascending order from a ASL formatted dataset
-        """
-        
-        # Define work variables
-        imgz_file_list = []
-        time_list = []
-
-        #* Only works for EuRoC MAV format
-        agent_cam0_fld = exp_dir + "/" + agent_name + "/" + "cam0"
-        imgz_file_dir = agent_cam0_fld + "/" + "data" + "/"
-        imgz_file_list = natsort.natsorted(os.listdir(imgz_file_dir),reverse=False)
-        # print(len(img_file_list)) # Debug, checks the number of rgb images
-
-        # Extract timesteps from image names
-        for iox in imgz_file_list:
-            time_step = iox.split(".")[0]
-            time_list.append(time_step)
-            #print(time_step)
-
-        return imgz_file_dir, imgz_file_list, time_list
     # ****************************************************************************************
 
     # ****************************************************************************************
@@ -167,7 +138,7 @@ class MonoDriver(Node):
         
         if(msg.data == "ACK"):
             self.send_config = False
-            # self.subscribe_exp_ack_.destory() # TODO doesn't work 
+            # self.subscribe_exp_ack_.destroy() # TODO doesn't work 
     # ****************************************************************************************
     
     # ****************************************************************************************
@@ -184,29 +155,18 @@ class MonoDriver(Node):
     # ****************************************************************************************
     
     # ****************************************************************************************
-    def run_py_node(self, idx, imgz_name):
+    def camera_image_callback(self, msg):
+        """
+            Callback that receives camera images and forwards them
+        """
+        if not self.send_config:  # Only process after handshake complete
+            self.run_py_node(msg)
+            
+    def run_py_node(self, img_msg):
         """
             Master function that sends the RGB image message to the CPP node
         """
-
-        # Initialize work variables
-        img_msg = None # sensor_msgs image object
-
-        # Path to this image
-        img_look_up_path = self.imgz_seqz_dir  + imgz_name
-        timestep = float(imgz_name.split(".")[0]) # Kept if you use a custom message interface to also pass timestep value
-        self.frame_id = self.frame_id + 1  
-        #print(img_look_up_path)
-        # print(f"Frame ID: {frame_id}")
-
-        # Based on the tutorials
-        img_msg = self.br.cv2_to_imgmsg(cv2.imread(img_look_up_path), encoding="passthrough")
-        timestep_msg = Float64()
-        timestep_msg.data = timestep
-
-        # Publish RGB image and timestep, must be in the order shown below. I know not very optimum, you can use a custom message interface to send both
         try:
-            self.publish_timestep_msg_.publish(timestep_msg) 
             self.publish_img_msg_.publish(img_msg)
         except CvBridgeError as e:
             print(e)
@@ -217,8 +177,6 @@ class MonoDriver(Node):
 def main(args = None):
     rclpy.init(args=args) # Initialize node
     n = MonoDriver("mono_py_node") #* Initialize the node
-    rate = n.create_rate(20) # https://answers.ros.org/question/358343/rate-and-sleep-function-in-rclpy-library-for-ros2/
-    
     #* Blocking loop to initialize handshake
     while(n.send_config == True):
         n.handshake_with_cpp_node()
@@ -231,19 +189,11 @@ def main(args = None):
     print(f"Handshake complete")
 
     #* Blocking loop to send RGB image and timestep message
-    for idx, imgz_name in enumerate(n.imgz_seqz[n.start_frame:n.end_frame]):
-        try:
-            rclpy.spin_once(n) # Blocking we need a non blocking take care of callbacks
-            n.run_py_node(idx, imgz_name)
-            rate.sleep()
-
-            # DEBUG, if you want to halt sending images after a certain Frame is reached
-            if (n.frame_id>n.frame_stop and n.frame_stop != -1):
-                print(f"BREAK!")
-                break
+    try:
+        rclpy.spin(n) # Blocking we need a non blocking take care of callbacks
         
-        except KeyboardInterrupt:
-            break
+    except KeyboardInterrupt:
+        pass
 
     # Cleanup
     cv2.destroyAllWindows() # Close all image windows
